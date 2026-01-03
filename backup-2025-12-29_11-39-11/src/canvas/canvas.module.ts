2767ca4403624a179bca8bd9404104081cedb716
Scott A Whitney <whitneyscottasl@gmail.com>
2026-01-02 19:38:07 -0600

    Sync repository with local directory and apply gitignore updates

--- backup-2025-12-29_11-39-11/src/canvas/canvas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CanvasService } from './canvas.service';
-import { CanvasController } from './canvas.controller';
-
-@Module({
-  providers: [CanvasService],
-  controllers: [CanvasController]
-})
-export class CanvasModule {}
